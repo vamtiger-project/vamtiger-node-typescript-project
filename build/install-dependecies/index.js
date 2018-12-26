97f41bfe28d3a35d4e783855d7da2f675ef3cc87
Vamtiger Immortalles <vamtiger@vamtiger.com>
2018-12-26 18:22:46 +0200

    vamtiger-node-typescript-commit

--- build/install-dependecies/index.js
+++ build/install-dependecies/index.js
@@ -1,21 +1,28 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const vamtiger_bash_1 = require("vamtiger-bash");
+const main_1 = require("vamtiger-argv/build/main");
+const args = new main_1.default();
+const devDependencies = [
+    '@types/node',
+    'mocha',
+    '@types/mocha',
+    'chai',
+    '@types/chai',
+    'typescript',
+    'vamtiger-node-typescript-commit'
+];
+const bundleDevDependencies = [
+    'vamtiger-bundle-typescript',
+    'vamtiger-remove'
+];
 exports.default = async (params) => {
-    const workingDirectory = params.workingDirectory;
+    const { workingDirectory } = params;
     const bashOptions = {
         cwd: workingDirectory
     };
-    const dependencies = [
-        '@types/node',
-        'mocha',
-        '@types/mocha',
-        'chai',
-        '@types/chai',
-        'typescript',
-        'vamtiger-node-typescript-commit'
-    ].join(' ');
-    const install = await vamtiger_bash_1.default(`npm install --save-dev ${dependencies}`, bashOptions)
+    const dependencies = (args.has('bundle') && devDependencies.concat(bundleDevDependencies) || devDependencies).join(' ');
+    await vamtiger_bash_1.default(`npm install --save-dev ${dependencies}`, bashOptions)
         .catch(error => console.warn(error));
 };
 //# sourceMappingURL=index.js.map
--- build/update-package/index.js
+++ build/update-package/index.js
@@ -2,15 +2,26 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const js_beautify_1 = require("js-beautify");
 const vamtiger_create_file_1 = require("vamtiger-create-file");
+const main_1 = require("vamtiger-argv/build/main");
+const args = new main_1.default();
 exports.default = async (params) => {
     const projectPackage = params.projectPackage;
     const currentPackage = require(projectPackage);
     let updatedPackage;
     currentPackage.main = 'build';
     currentPackage.types = 'build/index.d.ts';
-    currentPackage.scripts.test = 'mocha build/test --recursive';
+    currentPackage.scripts.test = 'npm run build && npm run mocha';
+    currentPackage.scripts.mocha = 'mocha build/test --recursive';
     currentPackage.scripts.build = 'tsc';
     currentPackage.scripts.commit = 'vamtigerNodeTypescriptCommit --push --publish';
+    if (args.has('bundle')) {
+        currentPackage.scripts.commit = 'npm run bundle && vamtiger-node-typescript-commit --push --publish --buildScript bundle';
+        currentPackage.scripts.clean = 'rm -rf build/*';
+        currentPackage.scripts['clean-bundle'] = 'vamtiger-remove -d build -p \\"(index|bin)\\\\.(js(\\\\.map)?|d\\\\.ts)\\"';
+        currentPackage.scripts['bundle-source'] = 'vamtiger-bundle-typescript --relativePath --entryFilePath source/index.ts --bundleFilePath build/index.js --format cjs --sourcemap true --copySourceMap --minify';
+        currentPackage.scripts.bundle = 'npm test && npm run clean && npm run build && npm run bundle-source && npm run clean-bundle';
+        currentPackage.scripts.watch = 'npm run bundle -- --watch';
+    }
     updatedPackage = js_beautify_1.js_beautify(JSON.stringify(currentPackage));
     await vamtiger_create_file_1.default(projectPackage, updatedPackage);
 };
