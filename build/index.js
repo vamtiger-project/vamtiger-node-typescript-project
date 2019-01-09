29c46b801c86b6113faff8815220ab9092d933a8
Vamtiger Immortalles <vamtiger@vamtiger.com>
2019-01-09 22:17:26 +0200

    vamtiger-node-typescript-commit

--- build/index.js
+++ build/index.js
@@ -1,7 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const path_1 = require("path");
-const main_1 = require("vamtiger-argv/build/main");
 const vamtiger_create_directory_1 = require("vamtiger-create-directory");
 const vamtiger_create_file_1 = require("vamtiger-create-file");
 const vamtiger_copy_file_1 = require("vamtiger-copy-file");
@@ -9,7 +8,6 @@ const initialize_repository_1 = require("./initialize-repository");
 const initialize_package_1 = require("./initialize-package");
 const update_package_1 = require("./update-package");
 const install_dependecies_1 = require("./install-dependecies");
-const args = new main_1.default();
 exports.default = async () => {
     const workingDirectory = process.cwd();
     const sourceBranch = 'source';
@@ -26,8 +24,6 @@ exports.default = async () => {
     await vamtiger_create_directory_1.default(sourceFolder);
     await initialize_package_1.default();
     await update_package_1.default({ projectPackage });
-    console.log(args.raw);
-    console.log({ "args.has('keepAlive')": args.has('keepAlive') });
     await Promise.all([
         initialize_repository_1.default({ workingDirectory }),
         vamtiger_create_directory_1.default(testFolder),
@@ -35,7 +31,6 @@ exports.default = async () => {
         install_dependecies_1.default({ workingDirectory }),
         vamtiger_copy_file_1.default(tsconfig)
     ]);
-    console.log({ "args.has('keepAlive')": args.has('keepAlive') });
     return true;
 };
 //# sourceMappingURL=index.js.map
