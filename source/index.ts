8f338fbfca13d6052f644f00886c5804602e3261
Vamtiger Immortalles <vamtiger@vamtiger.com>
2019-01-09 22:17:12 +0200

    vamtiger-node-typescript-commit

--- source/index.ts
+++ source/index.ts
@@ -1,5 +1,4 @@
 import { resolve as resolvePath, basename } from 'path';
-import Args from 'vamtiger-argv/build/main';
 import createDirectory from 'vamtiger-create-directory';
 import createFile from 'vamtiger-create-file';
 import copyFile from 'vamtiger-copy-file';
@@ -7,8 +6,6 @@ import initializeRepository from './initialize-repository';
 import initializePackage from './initialize-package';
 import updatePackage from './update-package';
 import installDependecies from './install-dependecies';
-
-const args = new Args();
 
 export default async () => {
     const workingDirectory = process.cwd();
@@ -49,9 +46,6 @@ export default async () => {
 
     await updatePackage({ projectPackage });
 
-    console.log(args.raw);
-    console.log({ "args.has('keepAlive')": args.has('keepAlive') });
-
     await Promise.all([
         initializeRepository({ workingDirectory }),
         createDirectory(testFolder),
@@ -59,8 +53,6 @@ export default async () => {
         installDependecies({ workingDirectory }),
         copyFile(tsconfig)
     ]);
-
-    console.log({ "args.has('keepAlive')": args.has('keepAlive') });
 
     return true;
 }
