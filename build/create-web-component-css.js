253452042196e295941b774010de654afe8a9346
James-Alex Matthews <vamtiger@vamtiger.com>
2019-02-19 23:29:01 +0200

    vamtiger-node-typescript-commit

--- build/create-web-component-css.js
+++ build/create-web-component-css.js
@@ -4,13 +4,12 @@ const path_1 = require("path");
 const vamtiger_create_directory_1 = require("vamtiger-create-directory");
 const vamtiger_create_file_1 = require("vamtiger-create-file");
 const { cwd } = process;
-const { all: parallel } = Promise;
 const folder = path_1.resolve(cwd(), 'source', 'css');
 const cssPath = path_1.resolve(folder, 'index.css');
 const documentCssPath = path_1.resolve(folder, 'document-index.css');
 async function default_1() {
     await vamtiger_create_directory_1.default(folder);
-    await parallel([
+    await Promise.all([
         vamtiger_create_file_1.default(cssPath, ''),
         vamtiger_create_file_1.default(documentCssPath, '')
     ]);
