b498ef07c65713ca212f6f37964635fa1e11bd0f
James-Alex Matthews <vamtiger@vamtiger.com>
2019-02-19 22:55:50 +0200

    Saving

---
+++ build/create-web-component-root.d.ts
@@ -0,0 +1,4 @@
+export default function ({ name }: IParams): Promise<void>;
+export interface IParams {
+    name: string;
+}
---
+++ build/create-web-component-root.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const path_1 = require("path");
+const vamtiger_create_directory_1 = require("vamtiger-create-directory");
+const vamtiger_create_file_1 = require("vamtiger-create-file");
+const { cwd } = process;
+const folder = path_1.resolve(cwd(), 'source');
+async function default_1({ name = 'index.ts' }) {
+    const filePath = path_1.resolve(folder, name);
+    await vamtiger_create_directory_1.default(folder);
+    await vamtiger_create_file_1.default(filePath, '');
+}
+exports.default = default_1;
+//# sourceMappingURL=create-web-component-root.js.map
