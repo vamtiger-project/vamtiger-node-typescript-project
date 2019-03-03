660a290f25de2660549b36f179933430910f0f6d
James-Alex Matthews <vamtiger@vamtiger.com>
2019-03-03 15:04:27 +0200

    vamtiger-node-typescript-commit

--- build/snippet/webcomponent-element.js
+++ build/snippet/webcomponent-element.js
@@ -1,10 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const lodash_1 = require("lodash");
-exports.default = ({ name }) => `import {
+exports.default = ({ name }) => `
+import loadScript from '../node_modules/vamtiger-browser-method/source/load-script';
+
+import {
     Selector,
     ObservedAttribute
 } from './types';
+import css from './css/document-index';
 import getTemplate from './get-template';
 import {
     shadowRoot as shadowRootConfig,
@@ -12,6 +16,9 @@ import {
 } from './config';
 
 export const name = '${lodash_1.kebabCase(name)}';
+
+css && loadScript({ name, css })
+    .catch(console.error);
 
 export default class ${lodash_1.startCase(name).replace(/\s+/g, '')} extends HTMLElement {
     constructor() {
@@ -39,5 +46,5 @@ export default class ${lodash_1.startCase(name).replace(/\s+/g, '')} extends HTM
     attributeChangedCallback(name: ObservedAttribute, oldValue: string, newValue: string) {
 
     }
-}`;
+}`.trim();
 //# sourceMappingURL=webcomponent-element.js.map
