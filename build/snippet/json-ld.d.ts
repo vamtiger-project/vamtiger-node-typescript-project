c2591fbdba6881010b5b6b6abd1a374ccd821454
James-Alex Matthews <vamtiger@vamtiger.com>
2019-05-06 01:19:26 +0200

    vamtiger-node-typescript-commit

--- build/snippet/json-ld.d.ts
+++ build/snippet/json-ld.d.ts
@@ -1,2 +1,2 @@
-declare const _default: "{\n    '@context': 'http://schema.org/',\n    '@type': '',\n};";
+declare const _default: "export default {\n    '@context': 'http://schema.org/',\n    '@type': '',\n};";
 export default _default;
--- build/snippet/json-ld.js
+++ build/snippet/json-ld.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = `{
+exports.default = `export default {
     '@context': 'http://schema.org/',
     '@type': '',
 };`;
